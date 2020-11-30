5c09bceb31eb9a807eac3efa7938e963de91b273
jaclyn barrera <jaclynnbarrera@gmail.com>
2020-11-30 19:21:58 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,16 +2,14 @@ def square_array(array)
     array.map {|el| el * el}
 end
 
-def summon_captain_planet(planeteer_calls)
-  planeteer_calls.collect_concat { |el| [el.capitalize + "!"] } 
+def summon_captain_planet(array)
+  array.collect_concat { |el| [el.capitalize + "!"] } 
 end
 
-def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.any? { |el| el.length >= 5}
+def long_planeteer_calls(array)
+  array.any? { |el| el.length >= 5}
 end
 
-def find_valid_calls(planeteer_calls)
-  valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  
-  planeteer_calls.find {|el| el == "Earth!" or el == "Wind!" or el == "Fire!" or el == "Water!" or el == "Heart!" }
+def find_valid_calls(array)
+  array.find {|el| el == "Earth!" or el == "Wind!" or el == "Fire!" or el == "Water!" or el == "Heart!" }
 end
